415241b04dfd45800c20840f94909373d0e4a6e9
U6L3UPZ35VORETN\Administrator <1002378395@qq.com>
2019-09-04 22:08:46 +0800

    新增子弹反弹

---
+++ Assets/BulletTest/BulletTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 反弹测试
+/// </summary>
+public class BulletTest : MonoBehaviour
+{
+
+    [SerializeField] private float m_BulletSpeed = 1f;
+
+    private bool isCanRebound = true;
+
+    void Start()
+    {
+      transform.LookAt(Vector3.zero);
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * m_BulletSpeed * Time.deltaTime, Space.Self);
+    }
+
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if ((LayerMask.LayerToName( other.gameObject.layer)=="Box"))
+        {
+            if (!isCanRebound)
+            {
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                ContactPoint point = other.contacts[0];
+                Vector3 curDir = transform.TransformDirection(Vector3.forward);
+                Vector3 nextDir = Vector3.Reflect(curDir, point.normal);
+                transform.rotation = Quaternion.FromToRotation(Vector3.forward, nextDir);
+                transform.localPosition = point.point;
+            }
+        }
+
+    }
+}
