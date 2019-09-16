b0eef3f11b057aa669738c1ab45ae82077df7681
U6L3UPZ35VORETN\Administrator <1002378395@qq.com>
2019-09-16 18:59:55 +0800

    增加linerenderer射线显示

---
+++ Assets/GameMain/Scenes/Test/LineRender/LineRenderTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineRenderTest : MonoBehaviour
+{
+
+    [SerializeField] private GameObject m_StartPoint;
+    [SerializeField] private GameObject m_TargetPoint;
+
+    private LineRenderer lineRenderer;
+    void Start()
+    {
+        if (lineRenderer == null)
+        {
+            lineRenderer = gameObject.AddComponent<LineRenderer>();
+        }
+
+        lineRenderer.startWidth = 0.1f;
+        lineRenderer.endWidth = 0.1f;
+
+        lineRenderer.SetPosition(0, m_StartPoint.transform.position);
+    }
+
+    void Update()
+    {
+        TargetMove();
+
+        lineRenderer.SetPosition(1, m_TargetPoint.transform.position);
+    }
+
+    void TargetMove()
+    {
+        m_TargetPoint.transform.Translate(Vector3.forward*Time.deltaTime,Space.World);
+    }
+}
